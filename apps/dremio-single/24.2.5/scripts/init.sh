01671f2dfd8003db1febe0d3f5794c391cecb38a
Node <8974108+qwenode@users.noreply.github.com>
2023-11-16 02:29:31 +0800

    init.sh 增加容错

--- apps/dremio-single/24.2.5/scripts/init.sh
+++ apps/dremio-single/24.2.5/scripts/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 source ./.env
-
+if [ -z "${MOUNT_PATH}" ] || [ -z "${CACHE_PATH}" ]; then
+    echo "Error: MOUNT_PATH or CACHE_PATH is empty. Exiting script."
+    exit 1
+fi
 mkdir -p ${MOUNT_PATH}
 mkdir -p ${CACHE_PATH}
 chown -R 999:999 ${MOUNT_PATH}
--- apps/elasticsearch-single/8.11.0/scripts/init.sh
+++ apps/elasticsearch-single/8.11.0/scripts/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 source ./.env
-
+if [ -z "${MOUNT_PATH}" ] || [ -z "${BACKUP_PATH}" ]; then
+    echo "Error: MOUNT_PATH or BACKUP_PATH is empty. Exiting script."
+    exit 1
+fi
 mkdir -p ${MOUNT_PATH}
 mkdir -p ${BACKUP_PATH}
 chown -R 1000:1000 ${MOUNT_PATH}
